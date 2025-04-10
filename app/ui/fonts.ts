2ae5942e46acf796459fe99999524cc96786b64d
Agustin Kinderknecht <agustinkcht@gmail.com>
2025-04-10 12:22:04 -0300

    Initial Commit - Pre Database

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+// export the font in a constant
+// const myFont = MyFont({ option: ['optionData'] })
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+
