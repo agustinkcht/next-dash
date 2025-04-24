878e13f708c17a9b94a92f50d14482aad0e323e7
Agustin Kinderknecht <133719441+agustinkcht@users.noreply.github.com>
2025-04-24 17:23:25 -0300

    Invoices CRUD + Error Handling (#6)
    - Server Actions used for the Invoices CRUD
    - Error Handling introduced using Error.tsx & not-found.tsx

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,10 +7,11 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -23,10 +24,13 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
-            >
-              <option value="" disabled>
+            > 
+            {/* "first option" as a placeholder for when no option is selected */}
+            {/* disabled so you can't choose it */}
+              <option value="" disabled>  
                 Select a customer
-              </option>
+              </option> 
+            {/* actual option display: */}
               {customers.map((customer) => (
                 <option key={customer.id} value={customer.id}>
                   {customer.name}
@@ -73,7 +77,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
-                  htmlFor="pending"
+                  htmlFor="pending" // associates the label via id
                   className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
                 >
                   Pending <ClockIcon className="h-4 w-4" />
