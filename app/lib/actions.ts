037519306548e15856524e8b29d51381738cedea
Agustin Kinderknecht <133719441+agustinkcht@users.noreply.github.com>
2025-04-30 18:13:16 -0300

    Sprint 12 - Form Validation for Invoices page (#7)
    ### Added Server-side Form Validation & Enhanced Accessibility to the Invoices page
    
    Main updates are in **edit-form.tsx**, **update-form.tsx**, and **actions.ts**:
    
    ### actions.ts
    * Used Zod to provide user-friendly, readable error messages for each field.
    * Both createInvoice and updateInvoice now use Zod's safeParse for validation, returning either a success object or a structured error object.
    
    ### edit-form.tsx & update-form.tsx
    * Introduced useActionState to manage form state and handle validation errors.
    * Error messages are now conditionally displayed below each field using the returned state.
    * Enhanced accessibility by using aria attributes to associate error messages with their respective fields.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,36 +4,55 @@ import { z } from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { State } from "./definitions";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 // define schemas with Caps
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than 0" }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status",
+  }),
   date: z.string(),
 });
 
 // sub-schema omitting data that will be generated apart or on db
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+  // if there is any field error, early return the errors
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Failed to create invoice due to missing fields",
+    };
+  }
+  // prepare the data for the query
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
+  // query the db
   try {
     await sql`
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 `;
   } catch (error) {
-    console.error(error);
+    return {
+      message: "Database Error: Failed to create Invoice",
+    };
   }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -43,12 +62,23 @@ export async function createInvoice(formData: FormData) {
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 // validate, format, change
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Failed to update invoice due to missing fields",
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   try {
     await sql`
@@ -57,7 +87,9 @@ export async function updateInvoice(id: string, formData: FormData) {
     WHERE id = ${id}
   `;
   } catch (error) {
-    console.error(error);
+    return {
+      message: "Database Error: Failed to update invoice"
+    }
   }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
